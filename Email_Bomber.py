0fd799b2aaa3c2e2141153224b4a9cb4bb8f65d9
EnzoZertio <57996264+EnzoZertio@users.noreply.github.com>
2019-12-23 14:38:01 +0000

    Add files via upload

--- Email_Bomber.py
+++ Email_Bomber.py
@@ -114,11 +114,11 @@ class Email_Bomber:
             print(f'ERROR: {e}')
 
     def attack(self):
-        print(bcolors.RED + '\n+[+[+[ Attaque en cours... ]+]+]+')
+        print(bcolors.Purple + '\n \~~~Attaque en cours...~~~/')
         for email in range(self.amount+1):
             self.send()
         self.s.close()
-        print(bcolors.RED + '\n+[+[+[ Attaque fini ]+]+]+')
+        print(bcolors.Purple + '\n \~~~Attaque finie~~~/')
         sys.exit(0)
 
 
